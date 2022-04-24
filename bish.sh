dd38361fe960a15bcffbabcec261bb2c4a8e6c19
Hardwaregore <hardwaregore101@gmail.com>
2022-04-24 11:39:12 -0500

    Update bish.sh

--- bish.sh
+++ bish.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-command -v php || echo "CRITICAL ERROR! ABORTING! (Err: 0x00a)"
-command -v curl || echo "CRITICAL ERROR! ABORTING! (Err: 0x01b)"
+command -v php || echo "CRITICAL ERROR! ABORTING! (Err: php NOT INSTALLED)"
+command -v curl || echo "CRITICAL ERROR! ABORTING! (Err: curl NOT INSTALLED)"
+command -v unzip || echo "CRITICAL ERROR! ABORTING! (Err: unzip NOT INSTALLED)"
+command -v wget || echo "CRITICAL ERROR! ABORTING! (Err: wget NOT INSTALLED)"
