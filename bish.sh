0d74276f9e4ffce58f116b822276b7ab848dd7db
Hardwaregore <hardwaregore101@gmail.com>
2022-04-24 11:51:43 -0500

    ah aegfbd.

--- bish.sh
+++ bish.sh
@@ -8,6 +8,6 @@ require_root() {
 }
 
 command -v php > /dev/null || echo "CRITICAL ERROR! ABORTING! (Err: php NOT INSTALLED)"
-command -v curl || echo "CRITICAL ERROR! ABORTING! (Err: curl NOT INSTALLED)"
-command -v unzip || echo "CRITICAL ERROR! ABORTING! (Err: unzip NOT INSTALLED)"
-command -v wget || echo "CRITICAL ERROR! ABORTING! (Err: wget NOT INSTALLED)"
+command -v curl > /dev/null|| echo "CRITICAL ERROR! ABORTING! (Err: curl NOT INSTALLED)"
+command -v unzip > /dev/null || echo "CRITICAL ERROR! ABORTING! (Err: unzip NOT INSTALLED)"
+command -v wget > /dev/null || echo "CRITICAL ERROR! ABORTING! (Err: wget NOT INSTALLED)"
