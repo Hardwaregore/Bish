d360ea0d3e00fa0fd4f4d58fd9db1c20af2b0532
Hardwaregore <hardwaregore101@gmail.com>
2022-04-24 12:07:39 -0500

    a

--- bish.sh
+++ bish.sh
@@ -16,7 +16,7 @@ require_root
 
 
 
-command -v php > /dev/null || printf "\n ${red}CRITICAL ERROR! ABORTING! (Err: NOT root)${reset} \n \n"
+command -v test > /dev/null || printf "\n ${red}CRITICAL ERROR! ABORTING! (Err: NOT root)${reset} \n \n"
 command -v curl > /dev/null|| printf "\n ${red}CRITICAL ERROR! ABORTING! (Err: NOT root)${reset} \n \n"
 command -v unzip > /dev/null || printf "\n ${red}CRITICAL ERROR! ABORTING! (Err: NOT root)${reset} \n \n"
 command -v wget > /dev/null || printf "\n ${red}CRITICAL ERROR! ABORTING! (Err: NOT root)${reset} \n \n"
