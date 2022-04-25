5fa78781a3d2e22afcabdf757ce5b00edde6ea48
Hardwaregore <hardwaregore101@gmail.com>
2022-04-25 17:04:57 -0500

    Update bish.sh

--- bish.sh
+++ bish.sh
@@ -24,19 +24,33 @@ command -v wget > /dev/null || printf "\n ${red}CRITICAL ERROR! ABORTING! (Err: 
 
 options() {
 clear
-printf "\n \n      ${yellow}[§GitHub§]${reset}    ${yellow}[§Google§]${reset}             ${yellow}[§GitLab]${reset} \n"
-printf "      ${yellow}[§Replit§]${reset}            ${yellow}[§Skyward§]${reset}         ${yellow}[§LightSpeedSystems§]${reset} \n"
-printf "      ${yellow}[§PythonAnywhere§]${reset}    ${yellow}[§Linode§]${reset}          ${yellow}[§AWS§]${reset} \n"
-printf "      ${yellow}[§MicrosoftAzure§]${reset}    ${yellow}[§DigitalOcean§]${reset}    ${yellow}[§AppleID§]${reset} \n"
+printf "\n \n      ${yellow}[§${green}GitHub${reset}${yellow}§]${reset}    ${yellow}[§${green}Google${reset}${yellow}§]${reset}             ${yellow}[§${green}GitLab${reset}${yellow}§]${reset} \n"
+printf "      ${yellow}[§${green}Replit${reset}${yellow}§]${reset}            ${yellow}[§${green}Skyward${reset}${yellow}§]${reset}         ${yellow}[§${green}LightSpeedSystems${reset}${yellow}§]${reset} \n"
+printf "      ${yellow}[§${green}PythonAnywhere${reset}§]${reset}    ${yellow}[§${green}Linode${reset}§]${reset}          ${yellow}[§${green}AWS${reset}§]${reset} \n"
+printf "      ${yellow}[§${green}MicrosoftAzure${reset}${yellow}§]${reset}    ${yellow}[§${green}DigitalOcean${reset}${yellow}§]${reset}    ${yellow}[§${green}AppleID${reset}${yellow}§]${reset} \n"
+
+
+}
+
+input() {
+read -p "Please choose an option $ " user_choice
+
+}
+
+input_processing() {
+
+
 
 
 }
 
 
-
 require_root
 required_packages
 options
+input
+
+
 
 
 
