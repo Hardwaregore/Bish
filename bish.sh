758688d9a4a4f90ec43bc399e4bce9e83edf67ef
Hardwaregore <hardwaregore101@gmail.com>
2022-04-26 21:33:32 -0500

    a

--- bish.sh
+++ bish.sh
@@ -39,11 +39,11 @@ read -p "Please choose an option $ " user_choice
 
 input_processing() {
 
-if [ user_choice = "Github" ]; then
+if [ user_choice == "Github" ]; then
 site = "GitHub"
 echo $site
 
-elif [ user_choice = Google ]; then
+elif [ user_choice == "Google" ]; then
 site = "Google"
 echo $site
 
